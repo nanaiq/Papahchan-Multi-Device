111524c939dfd46b9e5f5c8fcd83758f497bfb9e
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-11 19:43:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -92,8 +92,8 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '628'
-global.nomorown = '085641476033'
+global.nomorbot = '6285842769268'
+global.nomorown = '6285641476033'
 global.namebot = 'X-ZC'
 global.nameown = 'X'
 
