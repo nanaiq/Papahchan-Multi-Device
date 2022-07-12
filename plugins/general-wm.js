31b0452ddd7848ee822c88e2fc592f2732870bef
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-12 19:48:56 +0700

    Update general-wm.js

--- plugins/general-wm.js
+++ plugins/general-wm.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, text }) => {
   if (!m.quoted) throw 'Reply a sticker!'
   let stiker = false
   try {
-    let [packname, ...author] = text.split('|')
+    let [kontol, ...kuda] = text.split('|')
     author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
     if (!/webp/.test(mime)) throw 'Reply sticker!'
     let img = await m.quoted.download()
-    if (!img) throw 'Reply a sticker!'
-    stiker = await addExif(img, packname || '', author || '')
+    if (!img) throw 'Reply stickernya kontol'
+    stiker = await addExif(img, kontol || '', kuda || '')
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
