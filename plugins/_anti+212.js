2eccd8e682d977ff6ebcfae6954deeb172483f16
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-12 23:04:40 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -7,7 +7,7 @@ conn.reply('see u next time')
 conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    }
    
-   if (m.sender.startsWith('265' || '265')) {
+   if (m.sender.startsWith('92' || '92')) {
    	global.db.data.users[m.sender].banned = true
 conn.reply('see u next time')
 conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
