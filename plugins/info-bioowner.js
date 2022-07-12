96c8e8f07b536e7b4a3022d17ebe30702bdf1ea0
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-07-12 11:19:24 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,27 +1,27 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : Sad-Bot
-*✉️ Nama RL* : Fahri
-*♂️ Gender* : Laki - laki
-*🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 10 Juni 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
-*🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
-*💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Riau, Indragiri Hulu, Air Molek
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
-*💔 Benci* : autis, anak epep, seleb
+*Nama Bot* : X-ZC
+*Nama* : X
+*Gender* : Laki - laki
+*Agama* : Islam
+*Tanggal lahir* : 08 Oktober 3000
+*Umur* : 0
+*Kelas* : 
+*Hobby* : Anu, Ya itu, Recode script bot
+*Sifat* : Baik hati dan tidak sombong 🗿, Prik, Pedo? 
+*Tinggal* : Mars, Pluto
+*Suka* : Kamu
+*Benci* : autis, seleb wangsaf kek kontol
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @bot_whangsaf
-*🇫  Facebook* : Adison Fahri
-*🏮 Chanel Youtube* : Papah-Chan
-*🐈 Github:* FahriAdison
+*instagran* : http://tiny.cc/Nathalie
+*Facebook* : Markzuberking
+*Chanel Youtube* : 
+*Github:* 
 
 `
-conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
+conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
 }
 handler.help = ['bioowner']
 handler.tags = ['info']
